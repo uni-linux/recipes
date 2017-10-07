a1f3c967f5b5415faebf1fa7fe9682b16a930b2c
Dalton Menezes <daltonmenezes@outlook.com>
2017-10-07 06:31:07 -0300

    Update node-install recipe

--- src/daltonmenezes/node-install/ubuntu/recipe.sh
+++ src/daltonmenezes/node-install/ubuntu/recipe.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-node -v
-npm -v
+has_curl=$(which curl)
+
+function curlInstall {
+  sudo apt-get install curl -y
+}
+
+function nodeInstall {
+  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  clear
+}
+
+if [[ ! $has_curl ]]; then
+    curlInstall
+fi
+
+nodeInstall
+
+has_node=$(which node)
+
+if [[ $has_node ]]; then
+   node_version=`node -v`
+   npm_version=`npm -v`
+   printf "Node ${node_version} and NPM ${npm_version} were installed with success! :D\n"   
+else
+   printf "Sorry, the Node installation is failed. Please, try again. :(\n"
+fi
