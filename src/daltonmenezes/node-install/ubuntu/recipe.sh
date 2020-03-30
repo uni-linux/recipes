7dcb5a64e3cf8b8793f63e9ec990a7d5a25bd1ca
Dalton Menezes <daltonmenezes@outlook.com>
2020-03-30 13:29:41 -0300

    Update node-intall recipe to node 12.x

--- src/daltonmenezes/node-install/ubuntu/recipe.sh
+++ src/daltonmenezes/node-install/ubuntu/recipe.sh
@@ -6,7 +6,7 @@ function curlInstall {
 }
 
 function nodeInstall {
-  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   sudo apt-get install -y nodejs
   clear
 }
